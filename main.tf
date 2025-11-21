0e45f18d262ee8b7fad69ff328a68a68b1838053
Ben Meier <ben.meier@humanitec.com>
2025-11-21 11:58:37 +0000

    feat: initial commit

--- main.tf
+++ main.tf
@@ -1,1 +1,29 @@
-# Main TF code goes here
+resource "kubernetes_manifest" "route" {
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind       = "HTTPRoute"
+    metadata = {
+      namespace     = var.namespace
+      generate_name = var.name_prefix
+      annotations   = var.additional_annotations
+      labels        = var.additional_labels
+    }
+    spec = {
+      parentRefs = [for n in var.gateways : merge({ name = n }, var.gateway_namespace != null ? { namespace = var.gateway_namespace } : {})]
+      hostnames  = [var.hostname]
+      rules = [{
+        matches = [{
+          path = {
+            type  = var.path_match_type
+            value = var.path
+          }
+        }]
+        backendRefs = [{
+          name = split(".", var.service)[0]
+          port = var.service_port
+        }]
+      }]
+    }
+  }
+  computed_fields = ["metadata.name"]
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,79 @@
-# TF variables go here
+// REQUIRED
+
+variable "namespace" {
+  type        = string
+  description = "The namespace to create route in, this should be the same namespace as the backing service"
+}
+
+variable "path" {
+  type        = string
+  description = "The path to match"
+}
+
+variable "service" {
+  type        = string
+  description = "The service name to route traffic to within the cluster"
+}
+
+variable "service_port" {
+  type        = number
+  description = "The service port to route traffic to"
+}
+
+variable "hostname" {
+  type        = string
+  description = "The hostname to match traffic to this route"
+}
+
+variable "gateways" {
+  type        = list(string)
+  description = "The name of the Gateway API Gateways to connect this route to"
+
+  validation {
+    condition     = length(var.gateways) > 0
+    error_message = "Must define at least one gateway name"
+  }
+}
+
+// OPTIONALS
+
+variable "additional_annotations" {
+  type        = map(string)
+  description = "Additional annotations to add to the service account."
+  default     = {}
+}
+
+variable "additional_labels" {
+  type        = map(string)
+  description = "Additional labels to add to the service account."
+  default     = {}
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "The name prefix to use for the generated http route"
+  default     = "route-"
+}
+
+variable "gateway_namespace" {
+  type        = string
+  description = "The namespace of the var.gateways"
+  default     = null
+}
+
+variable "path_match_type" {
+  type        = string
+  default     = "Prefix"
+  description = "The path match strategy"
+
+  validation {
+    condition     = var.path_match_type == "Prefix" || var.path_match_type == "Exact" || var.path_match_type == "RegularExpression"
+    error_message = "path_match_type must be Prefix, Exact, or RegularExpression"
+  }
+}
+
+variable "filters" {
+  type        = list(any)
+  description = "Additional Gateway HTTPRoute filters to inject on this route"
+  default     = []
+}
